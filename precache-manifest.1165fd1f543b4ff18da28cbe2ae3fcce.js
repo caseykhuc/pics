434d7d016a1d76c6168cf29db8ae495ccc286e65
trangkhuc <khuctrang1812@gmail.com>
2019-10-19 00:55:27 -0400

    Updates

---
+++ precache-manifest.1165fd1f543b4ff18da28cbe2ae3fcce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b8629fa0dc128d280c7a3086a9728b",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "b5ec871b59569434de4e",
+    "url": "/pics/static/css/main.cae99229.chunk.css"
+  },
+  {
+    "revision": "72cee002eaa1c82a990e",
+    "url": "/pics/static/js/2.70573f3b.chunk.js"
+  },
+  {
+    "revision": "b5ec871b59569434de4e",
+    "url": "/pics/static/js/main.92e70de7.chunk.js"
+  },
+  {
+    "revision": "9878f3621a56e23127e1",
+    "url": "/pics/static/js/runtime-main.0a93e887.js"
+  }
+]);
